8e3ee85020124e73309b843e4fa773575c0c1296
nachocaceres <nachocaceres99@gmail.com>
2022-08-10 18:25:25 -0300

    2: Actualizacion de carrito

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 let libros = [
-  { titulo: "Estructura e interpretación de programas informáticos", autor: "Harold Abelson, Gerald Jay Sussman, Julie Sussman", imagen: "./assets/libro0.jpg", precio: 1100, id:1 },
-  { titulo: "Clean Code", autor: "Robert C. Martin", imagen: "./assets/libro1.jpg", precio: 950, id:2 },
-  { titulo: "No me hagas pensar", autor: "Steve Krug", imagen: "./assets/libro2.jpg", precio: 1900, id:3 },
-  { titulo: "Introducción a los algoritmos", autor: "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford", imagen: "./assets/libro3.jpg", precio: 1450, id:4 },
-  { titulo: "Código completo: Un manual práctico de construcción de software", autor: "Steve McConnell", imagen: "./assets/libro4.jpg", precio: 3000, id:5},
-  { titulo: "Patrones de diseño de Head First: Una guía para el cerebro", autor: "Eric Freeman, Bert Bates, Kathy Sierra, Elisabeth Robson", imagen: "./assets/libro5.jpg", precio: 920, id:6 },
-  { titulo: "Soft Skills: el manual de vida del desarrollador de software", autor: "John Sonmez", imagen: "./assets/libro6.jpg", precio: 1350, id:7},
-  { titulo: "El arte de la programación informática", autor: "Donald E. Knuth", imagen: "./assets/libro7.jpg", precio: 1200, id:8 }
+  { titulo: "Estructura e interpretación de programas informáticos", autor: "Harold Abelson, Gerald Jay Sussman, Julie Sussman", imagen: "./assets/libro0.jpg", precio: 1100, id: 1, cantidad:1 },
+  { titulo: "Clean Code", autor: "Robert C. Martin", imagen: "./assets/libro1.jpg", precio: 950, id: 2, cantidad:1 },
+  { titulo: "No me hagas pensar", autor: "Steve Krug", imagen: "./assets/libro2.jpg", precio: 1900, id: 3, cantidad:1 },
+  { titulo: "Introducción a los algoritmos", autor: "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford", imagen: "./assets/libro3.jpg", precio: 1450, id: 4, cantidad:1 },
+  { titulo: "Código completo: Un manual práctico de construcción de software", autor: "Steve McConnell", imagen: "./assets/libro4.jpg", precio: 3000, id: 5, cantidad:1 },
+  { titulo: "Patrones de diseño de Head First: Una guía para el cerebro", autor: "Eric Freeman, Bert Bates, Kathy Sierra, Elisabeth Robson", imagen: "./assets/libro5.jpg", precio: 920, id: 6, cantidad:1 },
+  { titulo: "Soft Skills: el manual de vida del desarrollador de software", autor: "John Sonmez", imagen: "./assets/libro6.jpg", precio: 1350, id: 7, cantidad:1 },
+  { titulo: "El arte de la programación informática", autor: "Donald E. Knuth", imagen: "./assets/libro7.jpg", precio: 1200, id: 8, cantidad:1 }
 ]
 
 // ITEMS / LIBROS A LA VENTA EN CARDS
 
-const mostrarLibros = (libros) =>{
-  
+const mostrarLibros = (libros) => {
+
   const contenedorLibros = document.getElementById("contenedor-libros");
-  
+
   libros.forEach((libro) => {
     const div = document.createElement("div");
     div.classList.add("col");
@@ -28,16 +28,16 @@ const mostrarLibros = (libros) =>{
     Rog</span> <span class="float-end price-hp">$ ${libro.precio}</span> </div>
     <h5 class="card-title">${libro.titulo}</h5>
     <p class="card-text">Autor: ${libro.autor}</p>
-    <button id="agregar${libro.id}" class="text-center my-4 boton-agregar"> <a href="#" class="btn btn-warning">Agregar</a> </button>
+    <button id="agregar${libro.id}" class="text-center my-4 boton-agregar"> <a class="btn btn-warning">Agregar</a> </button>
     
     </div>
     </div>
     </div>
     `
     contenedorLibros.appendChild(div);
-    
+
     const boton = document.getElementById(`agregar${libro.id}`);
-    boton.addEventListener("click", ()=>{
+    boton.addEventListener("click", () => {
       agregarCarrito(libro.id)
     })
   })
@@ -47,18 +47,56 @@ mostrarLibros(libros)
 
 //CARRITO DE COMPRAS
 
-const carrito = []
+let carrito = []
 
-const agregarCarrito = (libroId)=>{
-  const item = libros.find((libro) => libro.id == libroId);
-  carrito.push(item);
+const botonVaciar = document.getElementById("vaciar-carrito")
+
+const contadorCarrito = document.getElementById("contador-carrito")
+
+const precioTotal = document.getElementById("total")
+
+const vaciarCarrito = document.getElementById("vaciar")
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("carrito")) {
+    carrito = JSON.parse(localStorage.getItem("carrito"))
+    actCarrito()
+  }
+})
+
+vaciarCarrito.addEventListener("click", () => {
+  carrito.length = 0;
+  actCarrito()
+})
+
+const agregarCarrito = (libroId) => {
+  const existe = carrito.some(libro => libro.id == libroId) //comprobar si el elemento ya existe en el carro
+
+  if (existe) { //SI YA ESTÁ EN EL CARRITO, ACTUALIZAMOS LA CANTIDAD
+    const libro = carrito.map(libro => { //creamos un nuevo arreglo e iteramos sobre cada curso y cuando
+      // map encuentre cual es el q igual al que está agregado, le suma la cantidad
+      if (libro.id == libroId) {
+        libro.cantidad++
+      }
+    })
+  } else {
+    const item = libros.find((libro) => libro.id == libroId);
+    carrito.push(item);
+  }
   actCarrito()
   console.log(carrito);
 }
 
 const contenedorCarrito = document.getElementById("carrito-contenedor")
 
-const actCarrito = () => { 
+const eliminarCarrito = (libroId) => {
+  const item = carrito.find((libro) => libro.id == libroId)
+  const index = carrito.indexOf(item)
+  carrito.splice(index, 1)
+  actCarrito()
+}
+
+const actCarrito = () => {
   contenedorCarrito.innerHTML = ""
   carrito.forEach((libro) => {
     const div = document.createElement("div");
@@ -71,33 +109,34 @@ const actCarrito = () => {
     <button onclick = "eliminarCarrito(${libro.id})" class="btnDlt">X</button>
     <hr>
     `
-    contenedorCarrito.appendChild(div)
-  })
-}
 
-const eliminarCarrito = (libroId) => {
-  const item = carrito.find((libro) => libro.id == libroId)
-  const index = carrito.indexOf(item)
-  carrito.splice(index, 1)
-  actCarrito()
-}
-  
+    contenedorCarrito.appendChild(div)
+
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+  })  
+  contadorCarrito.innerText = carrito.length
+  precioTotal.innerText = carrito.reduce((acc, libro) => acc + (libro.cantidad * libro.precio), 0)
+}  
+
+
+
 //MODAL
-if(document.getElementById("btnModal")){
+if (document.getElementById("btnModal")) {
   var modal = document.getElementById("myModal");
   var btn = document.getElementById("btnModal");
   var span = document.getElementsByClassName("close")[0];
   var body = document.getElementsByTagName("body")[0];
-  
-  btn.onclick = function() {
+
+
+  btn.onclick = function () {
     modal.style.display = "block";
-    
+
     body.style.position = "static";
     body.style.height = "100%";
     body.style.overflow = "hidden";
   }
 
-  span.onclick = function() {
+  span.onclick = function () {
     modal.style.display = "none";
 
     body.style.position = "inherit";
@@ -105,7 +144,7 @@ if(document.getElementById("btnModal")){
     body.style.overflow = "visible";
   }
 
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
 
@@ -115,3 +154,4 @@ if(document.getElementById("btnModal")){
     }
   }
 }
+
