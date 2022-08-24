fc792253783baf2d6296de44b1c6b26e182de430
nachocaceres <nachocaceres99@gmail.com>
2022-08-24 12:36:42 -0300

    Aplicacion de fetch

--- js/script.js
+++ js/script.js
@@ -1,55 +1,51 @@
-let libros = [
-  { titulo: "Estructura e interpretación de programas informáticos", autor: "Harold Abelson, Gerald Jay Sussman, Julie Sussman", imagen: "./assets/libro0.jpg", precio: 1100, id: 1, cantidad:1 },
-  { titulo: "Clean Code", autor: "Robert C. Martin", imagen: "./assets/libro1.jpg", precio: 950, id: 2, cantidad:1 },
-  { titulo: "No me hagas pensar", autor: "Steve Krug", imagen: "./assets/libro2.jpg", precio: 1900, id: 3, cantidad:1 },
-  { titulo: "Introducción a los algoritmos", autor: "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford", imagen: "./assets/libro3.jpg", precio: 1450, id: 4, cantidad:1 },
-  { titulo: "Código completo: Un manual práctico de construcción de software", autor: "Steve McConnell", imagen: "./assets/libro4.jpg", precio: 3000, id: 5, cantidad:1 },
-  { titulo: "Patrones de diseño de Head First: Una guía para el cerebro", autor: "Eric Freeman, Bert Bates, Kathy Sierra, Elisabeth Robson", imagen: "./assets/libro5.jpg", precio: 920, id: 6, cantidad:1 },
-  { titulo: "Soft Skills: el manual de vida del desarrollador de software", autor: "John Sonmez", imagen: "./assets/libro6.jpg", precio: 1350, id: 7, cantidad:1 },
-  { titulo: "El arte de la programación informática", autor: "Donald E. Knuth", imagen: "./assets/libro7.jpg", precio: 1200, id: 8, cantidad:1 }
-]
-
 // ITEMS / LIBROS A LA VENTA EN CARDS
+fetch("./data/libros.json")
+  .then((response) =>response.json())
+  .then((data)=>{
+    libros = data;
+    console.log(libros)
+    const mostrarLibros = (libros) => {
 
-const mostrarLibros = (libros) => {
-
-  const contenedorLibros = document.getElementById("contenedor-libros");
-
-  libros.forEach((libro) => {
-    const div = document.createElement("div");
-    div.classList.add("col");
-    div.innerHTML = ` 
-    <div class="col">
-    <div class="card h-100 shadow-sm"> <img
-    src="${libro.imagen}"
-    class="card-img-top" alt="Foto del libro">
-    <div class="card-body">
-    <div class="clearfix mb-3"> <span class="float-start badge rounded-pill bg-primary"><i class="fa-solid fa-book"></i>
-    Book</span> <span class="float-end price-hp">$ ${libro.precio}</span> </div>
-    <h5 class="card-title">${libro.titulo}</h5>
-    <p class="card-text">Autor: ${libro.autor}</p>
-    <button id="agregar${libro.id}" type="button" class="btn btn-primary btn-lg text-center my-4 boton-agregar">Agregar <i class="fa-duotone fa-cart-arrow-up"></i></button>
-    </div>
-    </div>
-    </div>
-    `
-    contenedorLibros.appendChild(div);
-
-    const boton = document.getElementById(`agregar${libro.id}`);
-    boton.addEventListener("click", () => {
-      agregarCarrito(libro.id)
-      Toastify({
-        text: `El libro " ${libro.titulo} " fue agregado correctamente`,
-        className: "info",
-        style: {
-          background: "#83b06a",
-        }
-      }).showToast();
-    })
+      const contenedorLibros = document.getElementById("contenedor-libros");
+    
+      libros.forEach((libro) => {
+        const div = document.createElement("div");
+        div.classList.add("col");
+        div.innerHTML = ` 
+        <div class="col">
+        <div class="card h-100 shadow-sm"> <img
+        src="${libro.imagen}"
+        class="card-img-top" alt="Foto del libro">
+        <div class="card-body">
+        <div class="clearfix mb-3"> <span class="float-start badge rounded-pill bg-primary"><i class="fa-solid fa-book"></i>
+        Book</span> <span class="float-end price-hp">$ ${libro.precio}</span> </div>
+        <h5 class="card-title">${libro.titulo}</h5>
+        <p class="card-text">Autor: ${libro.autor}</p>
+        <button id="agregar${libro.id}" type="button" class="btn btn-primary btn-lg text-center my-4 boton-agregar">Agregar <i class="fa-duotone fa-cart-arrow-up"></i></button>
+        </div>
+        </div>
+        </div>
+        `
+        contenedorLibros.appendChild(div);
+    
+        const boton = document.getElementById(`agregar${libro.id}`);
+        boton.addEventListener("click", () => {
+          agregarCarrito(libro.id)
+          Toastify({
+            text: `El libro " ${libro.titulo} " fue agregado correctamente`,
+            className: "info",
+            style: {
+              background: "#83b06a",
+            }
+          }).showToast();
+        })
+      })
+    }
+    
+    mostrarLibros(libros)
   })
-}
 
-mostrarLibros(libros)
+
 
 //CARRITO DE COMPRAS
 
